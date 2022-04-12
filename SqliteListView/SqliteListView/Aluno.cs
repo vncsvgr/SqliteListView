b24ff6c60e54c06378c91b7f46ea17fb42529050
Usuario <Usuario@CPD03-M12>
2022-04-11 22:35:56 -0300

    Adicionar arquivos de projeto.

---
+++ SqliteListView/SqliteListView/Aluno.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SQLite;
+
+namespace SqliteListView
+{
+    public class Aluno
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Curso { get; set; }
+
+        SQLiteConnection db;
+
+        public Aluno()
+        {
+            db = new SQLiteConnection(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "escola.db"));
+            db.CreateTable<Aluno>();
+        }
+
+        public void Adicionar(Aluno a)
+        {
+            db.Insert(a);
+        }
+
+        public void Atualizar(Aluno a)
+        {
+            db.Update(a);
+        }
+
+        public void Excluir(Aluno a)
+        {
+            db.Delete(a);
+        }
+
+        public List<Aluno> Listar(string nome)
+        {
+            return db.Table<Aluno>().Where(i => i.Nome.ToUpper().Contains(nome.ToUpper())).ToList();
+        }
+    }
+}
---
+++ SqliteListView/SqliteListView/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SqliteListView
+{
+    public partial class MainPage : ContentPage
+    {
+        Aluno a = new Aluno();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            lstAlunos.ItemsSource = a.Listar("");
+        }
+
+        void Limpar()
+        {
+            txtId.Text = String.Empty;
+            txtNome.Text = String.Empty;
+            txtCurso.Text = String.Empty;
+        }
+
+        private void btnAdicionar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNome.Text))
+            {
+                DisplayAlert("Atenção", "Por favor insira um nome", "OK");
+                return;
+            }
+
+            a = new Aluno
+            {
+                Nome = txtNome.Text,
+                Curso = txtCurso.Text
+            };
+
+            a.Adicionar(a);
+            lstAlunos.ItemsSource = a.Listar("");
+            Limpar();
+        }
+
+        private void btnAtualizar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtId.Text))
+            {
+                DisplayAlert("Atenção", "Por favor insira um ID", "OK");
+                return;
+            }
+
+            a = new Aluno
+            {
+                Id = int.Parse(txtId.Text),
+                Nome = txtNome.Text,
+                Curso = txtCurso.Text
+            };
+
+            a.Atualizar(a);
+            lstAlunos.ItemsSource = a.Listar("");
+            Limpar();
+        }
+
+        private void btnExcluir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sbAluno_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lstAlunos.ItemsSource = a.Listar(sbAluno.Text);
+        }
+
+        private void lstAlunos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            a = e.SelectedItem as Aluno;
+
+            txtId.Text = a.Id.ToString();
+            txtNome.Text = a.Nome;
+            txtCurso.Text = a.Curso;
+        }
+
+        private void btnFinalizar_Clicked(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
